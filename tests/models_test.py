b41c4cb95b556e59a0c1a7f22caf8125ed72199b
Gabriel Oduori <gabriel.oduori@gmail.com>
2019-04-12 14:20:12 +0300

    Testing Created a unit test for the two classes in the model.py file. These tests are to check if the bevaiours of both Source and Article classes

--- tests/models_test.py
+++ tests/models_test.py
@@ -1,5 +1,8 @@
+# import os
 import unittest
-from app.models import Sources,Articles
+from models import Source, Article
+
+# sys.path.append('./app')
 
 
 class SourceTest(unittest.TestCase):
@@ -12,8 +15,29 @@ class SourceTest(unittest.TestCase):
         Set up method that will run before every Test
         '''
         
-        self.new_source = Source()
+        self.new_source = Source('BBC', 'Another Crazy News', 'Gabriel Oduori', '10.12.2019','This is one greate news we need to look at','https://i.kinja-img.com/gawker-media/image/upload/s--FSk-nXyV--/c_fill,fl_progressive,g_center,h_900,q_80,w_1600/wihsmbwlreqfqviq7fdc.jpg','https://techcrunch.com/2019/04/11/rasa-raises-13m-led-by-accel-for-its-developer-friendly-open-source-approach-to-chatbots/')
         
     def test_instance(self):
         self.assertTrue(isinstance(self.new_source, Source))
+        
+
+
+class ArticleTest(unittest.TestCase):
+    '''
+    Test case to test behavior of teh Article class
+    ''' 
+    
+    def setUp(self):
+        '''
+        Set up method that will run before every Test
+        '''
+        
+        self.new_article = Article('BBC', 'Crazy Article', 'Oduori Gabriel', '13.04.2019','https://i.kinja-img.com/gawker-media/image/upload/s--FSk-nXyV--/c_fill,fl_progressive,g_center,h_900,q_80,w_1600/wihsmbwlreqfqviq7fdc.jpg','content')
+        
+    def test_instance(self):
+        self.assertTrue(isinstance(self.new_article,Article))
+        
+        
+if __name__ == "__main__":
+    unittest.main()
 
