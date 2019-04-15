ee18848508697012b93b72926960f60cfea62ae1
Gabriel Oduori <gabriel.oduori@gmail.com>
2019-04-15 10:38:53 +0300

    News Sources
    Updated the news sources to include general, science and technology categories on the index.html and the views files

--- app/main/views.py
+++ app/main/views.py
@@ -10,8 +10,20 @@ def index():
     business_sources = get_sources('business')
     entertainemnt_sources = get_sources('entertainment')
     science_sources = get_sources('science')
+    sports_sources = get_sources('sports')
+    general_sources = get_sources('general')
+    technology_sources = get_sources('technology')
+    
     headline = 'Home of news highlights'
-    return render_template('index.html', headline=headline, business=business_sources, entertainment = entertainemnt_sources, science = science_sources)
+    return render_template('index.html', 
+                           headline=headline, 
+                           business=business_sources, 
+                           entertainment = entertainemnt_sources, 
+                           science = science_sources,
+                           sports = sports_sources,
+                           technology = technology_sources,
+                           general = general_sources
+                           )
 
 
 @main.route('/highlights')
@@ -23,4 +35,16 @@ def top_news():
 
     
     return render_template('articles.html',us=us_news, gb=uk_news )
+
+
+
+@main.route('/articles/<id>')
+def articles(id):
+    '''
+    this function takes in a list of scources anf filters them out by a specific
+    source using the source id. For example, if the source is BBC, it will take
+    in the "id" as bbc-news
+    '''
+    aricle = get_article(id)
     
+    return render_template('articles.html', id = source)
